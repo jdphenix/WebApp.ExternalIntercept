a9a83831e512a041bc05675af954e4de0ac80f3a
Justin Davis <jdphenix@gmail.com>
2016-06-14 22:02:55 -0500

    Initial commit

---
+++ WebApp.ExternalIntercept/Models/LeavingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.ExternalIntercept.Models
+{
+    public sealed class LeavingViewModel
+    {
+        public LeavingViewModel(string uri, Uri referrer = null)
+        {
+            this.ExternalUri = new Uri(uri);
+            this.Referrer = referrer;
+        }
+
+        public Uri ExternalUri { get; }
+        public Uri Referrer { get; }
+    }
+}
